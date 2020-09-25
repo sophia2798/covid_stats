188e14b1aee77c7fe5501f7b9c813655b00e903d
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-09-24 19:01:56 -0700

    finished adding graph modal after addressing conflicts

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ $(document).ready(function () {
 
     // Once search button is clicked, call storeInput()
     displayHistory();
+    $(document).on("click", ".collection-item", function (event) {
+        event.preventDefault();
+
+        // Get user input
+        cityID = event.target.id;
+        cityIndex = cityID[cityID.length - 1];
+        cityName = JSON.parse(localStorage.getItem("storageCity"))[cityIndex];
+        stateName = JSON.parse(localStorage.getItem("storageState"))[cityIndex];
+        // console.log(cityName, stateName);
+        ajaxCalls(cityName, stateName, getFullState(stateName));
+    });
+
     $("#submit-btn").on("click", storeInput);
 
     function displayHistory() {
@@ -17,8 +29,10 @@ $(document).ready(function () {
 
         // Display local storage city and state to history, format: Seattle, WA
         storageCityArray.forEach(function (cityEl, index) {
+            $("#hist" + index).css("display", "block");
             $("#hist" + index).text(cityEl + ", " + storageStateArray[index]);
             // console.log("check display");
+
         });
     }
 
@@ -34,21 +48,20 @@ $(document).ready(function () {
         // Display modal with error messages
         if (cityInput === "" && stateInput === "") {
             $("#error-message").text("Please enter a city and a state.");
+            displayModal();
 
         } else if (cityInput === "" && stateInput.length > 0) {
             $("#error-message").text("Please enter a city.");
+            displayModal();
 
         } else if (cityInput.length > 0 && stateInput === "") {
             $("#error-message").text("Please enter a state code.");
-            
+            displayModal();
+
         } else if (cityInput.length > 0 && stateInput.length != 2) {
             $("#error-message").text("Please enter a correct state code.");
+            displayModal();
         }
-
-        $(".error-modal").show();
-        $(".close").on("click", function () {
-            $(".modal").css("display", "none");
-        });
 
         // Change to uppercase
         cityInput = cityToUpperCase(cityInput);
@@ -91,142 +104,84 @@ $(document).ready(function () {
 
     }
 
-    // Function input: city to be changed to uppercase
-    // Function output: camel case city name
-    function cityToUpperCase(cityToChange) {
-        var cityUC = "";
-
-        cityToChange = cityToChange.split(" ");
-        // console.log(cityToChange);
-        cityToChange.forEach(function (cityPart) {
-            // console.log("before: ", cityPart);
-            cityPart = cityPart.charAt(0).toUpperCase() + cityPart.slice(1);
-            // console.log("after: ", cityPart);
-            cityUC = cityUC + " " + cityPart;
-            // console.log(cityUC);
+    function displayModal() {
+        $(".error-modal").show();
+        $(".close").on("click", function () {
+            $(".error-modal").css("display", "none");
         });
-        return cityUC.trim();
     }
 
-    $("#submit-btn").on("click", function (event) {
-        event.preventDefault();
-        $(".modal-trigger").show();
-        // Assign variables to input values
-        var cityName = $("#city-search").val().trim();
-        var stateName = ($("#state-search").val()).toUpperCase();
-        var fullStateName = getFullState(stateName);
-        console.log(cityName, stateName, fullStateName)
-        $("input").val("");
-
-        if (cityName.length > 0 && stateName.length === 2) {
-            // Lon and Lat API Call
-            $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?q="+cityName+","+fullStateName+"&appid=e0b82fbe866155125ec89e15985f0d60",
-                method: "GET"
-            }).then(function(response) {
-                var cityLatitude = response.coord.lat;
-                var cityLongitude = response.coord.lon;
-    
-                // Testing Center API Call
-                var testingCenterKey = "aUGtjGfxYZm_i4czjxJiqasqeMEkhvjaRig_VG6cUtA";
-                console.log(cityLongitude, cityLatitude)
-                // Limit to 5 results
-                var resultLimit = 5;
-                var testingCenterURL = "https://discover.search.hereapi.com/v1/discover?apikey=" + testingCenterKey + "&q=Covid&at=" + cityLatitude + "," + cityLongitude + "&limit=" + resultLimit;
-                
-                // All results
-                // var testingCenterURL = "https://discover.search.hereapi.com/v1/discover?apikey=" + testingCenterKey + "&q=Covid&at=" + cityLatitude + "," + cityLongitude;
-                var myCountyArray = []; // LC-Declaring Global County array to store county info of 5 testing centers.
-                $.ajax({
-                    url: testingCenterURL,
-                    method: "GET"
-                }).then(function (testingCenterResponse) {
-                    // console.log(testingCenterResponse);
-                    for (var i = 0; i < resultLimit; i++) {
-                        // console.log(testingCenterResponse.items[i]);
-                        // console.log(testingCenterResponse.items[i].title);
-                        // console.log(testingCenterResponse.items[i].address.label);
-                        // console.log(testingCenterResponse.items[i].contacts[0].phone[0].value);
-                        var address = testingCenterResponse.items[i].address.label.split(":")[1];
-                        // console.log(address); // ZW - commented it 
-                        $("#loc" + i).text(address);
-                        myCountyArray.push(testingCenterResponse.items[i].title, testingCenterResponse.items[i].address.county);//LC-This will push county information to myCountyArray
-                    }
-                });
-            });
-    
-            // Health Department API Call
-            
-            // Convert state name to all lower case and replace spaces with hyphens is applicable
-            if (fullStateName.indexOf(' ') >= 0) {
-                var healthDeptState = (fullStateName.toLowerCase()).replace(/\s/g, "-");
-            }
-            else {
-                var healthDeptState = fullStateName.toLowerCase();
-            };
-            
-            var healthDeptURL = "https://postman-data-api-templates.github.io/county-health-departments/api/"+healthDeptState+".json";
-    
-            $.ajax({
-                url: healthDeptURL,
-                method: "GET"
-            }).then(function (response) {
-                var myArray = response;
-                $.each(myArray, function (index, value) {
-                    // console.log(value.name); // ZW - commented it
-                    // console.log(value.address); // ZW - commented it 
-                    // console.log(value.website); // ZW - commented it 
-                })
-                // ZW - commented it 
-                // console.log("County list: ", myCountyArray);// LC-Proof that county info is now reachable within this ajax request. 
-            });
-    
-            // State Stats API
-            var stateURL = "https://api.covidtracking.com/v1/states/"+stateName+"/current.json";
-    
-            $.ajax({
-                url: stateURL,
-                method: "GET"
-            }).then(function (response) {
-                // console.log(response);
-                var dateString = response.date;
-                var dateFormat = moment(dateString, "YYYYMMDD").format('MMMM Do YYYY');
-                console.log(dateFormat)
-                var totalTested = (response.totalTestResults).toLocaleString('en');
-                var totalPos = (response.positive).toLocaleString('en');
-                var totalNeg = (response.negative).toLocaleString('en');
-                var currentHosp = (response.hospitalizedCurrently).toLocaleString('en');
-                var deaths = (response.death).toLocaleString('en');
-                $("#state-name").text(fullStateName);
-                $("#total-tested").text(totalTested);
-                $("#positive").text(totalPos);
-                $("#negative").text(totalNeg);
-                $("#hospitalized").text(currentHosp);
-                $("#deaths").text(deaths);
-                $("#update-date").text(dateFormat);
-                console.log(totalTested, totalPos, totalNeg, currentHosp, deaths);
-            });
-
-        }
-        
-        var healthDeptURL = "https://postman-data-api-templates.github.io/county-health-departments/api/"+healthDeptState+".json";
-
+    function ajaxCalls(cityName, stateName, fullStateName) {
+        // Lon and Lat API Call
         $.ajax({
-            url: healthDeptURL,
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + fullStateName + "&appid=e0b82fbe866155125ec89e15985f0d60",
             method: "GET"
         }).then(function (response) {
-            var myArray = response;
-            $.each(myArray, function (index, value) {
-                // console.log(value.name); // ZW - commented it
-                // console.log(value.address); // ZW - commented it 
-                // console.log(value.website); // ZW - commented it 
-            })
-            // ZW - commented it 
-            // console.log("County list: ", myCountyArray);// LC-Proof that county info is now reachable within this ajax request. 
-        });
+            var cityLatitude = response.coord.lat;
+            var cityLongitude = response.coord.lon;
 
+            // Testing Center API Call
+            var testingCenterKey = "aUGtjGfxYZm_i4czjxJiqasqeMEkhvjaRig_VG6cUtA";
+            // console.log(cityLongitude, cityLatitude)
+            // Limit to 5 results
+            var resultLimit = 5;
+            var testingCenterURL = "https://discover.search.hereapi.com/v1/discover?apikey=" + testingCenterKey + "&q=Covid&at=" + cityLatitude + "," + cityLongitude + "&limit=" + resultLimit;
+
+            // All results
+            // var testingCenterURL = "https://discover.search.hereapi.com/v1/discover?apikey=" + testingCenterKey + "&q=Covid&at=" + cityLatitude + "," + cityLongitude;
+            var myCountyArray = []; // LC-Declaring Global County array to store county info of 5 testing centers.
+            $.ajax({
+                url: testingCenterURL,
+                method: "GET"
+            }).then(function (testingCenterResponse) {
+                // console.log(testingCenterResponse);
+                for (var i = 0; i < resultLimit; i++) {
+                    // console.log(testingCenterResponse.items[i]);
+                    // console.log(testingCenterResponse.items[i].title);
+                    // console.log(testingCenterResponse.items[i].address.label);
+                    // console.log(testingCenterResponse.items[i].contacts[0].phone[0].value);
+                    var address = testingCenterResponse.items[i].address.label.split(":")[1];
+                    // console.log(address); // ZW - commented it 
+                    $("#loc" + i).css("display", "block");
+                    $("#loc" + i).text(address);
+
+                    // myCountyArray.push(testingCenterResponse.items[i].title, testingCenterResponse.items[i].address.county);//LC-This will push county information to myCountyArray
+                }
+                var countyName = testingCenterResponse.items[0].address.county;
+
+                // Health Department API Call
+
+                // Convert state name to all lower case and replace spaces with hyphens is applicable
+                if (fullStateName.indexOf(' ') >= 0) {
+                    var healthDeptState = (fullStateName.toLowerCase()).replace(/\s/g, "-");
+                }
+                else {
+                    var healthDeptState = fullStateName.toLowerCase();
+                };
+
+                var healthDeptURL = "https://postman-data-api-templates.github.io/county-health-departments/api/" + healthDeptState + ".json";
+
+                $.ajax({
+                    url: healthDeptURL,
+                    method: "GET"
+                }).then(function (response) {
+                    // console.log(countyName);
+                    // console.log(response);
+                    response.forEach(function (countyEl) {
+                        if (countyEl.name.includes(countyName)) {
+                            // Display county info
+                            $("#county-name").text(countyEl.name);
+                            $("#phone-number").text(countyEl.phone);
+                            $("#county-address").text(countyEl.address);
+                            $("#county-url").text("Website");
+                            $("#county-url").attr("href", countyEl.website);
+                        }
+                    });
+                });
+            });
+        });
         // State Stats API
-        var stateURL = "https://api.covidtracking.com/v1/states/"+stateName+"/current.json";
+        var stateURL = "https://api.covidtracking.com/v1/states/" + stateName + "/current.json";
 
         $.ajax({
             url: stateURL,
@@ -235,7 +190,7 @@ $(document).ready(function () {
             // console.log(response);
             var dateString = response.date;
             var dateFormat = moment(dateString, "YYYYMMDD").format('MMMM Do YYYY');
-            console.log(dateFormat)
+            // console.log(dateFormat)
             var totalTested = (response.totalTestResults).toLocaleString('en');
             var totalPos = (response.positive).toLocaleString('en');
             var totalNeg = (response.negative).toLocaleString('en');
@@ -248,9 +203,9 @@ $(document).ready(function () {
             $("#hospitalized").text(currentHosp);
             $("#deaths").text(deaths);
             $("#update-date").text(dateFormat);
+            // console.log(totalTested, totalPos, totalNeg, currentHosp, deaths);
         });
 
-        // Create Graph
         $.ajax({
             url: "https://api.covidtracking.com/v1/states/"+stateName+"/daily.json",
             method: "GET"
@@ -346,24 +301,59 @@ $(document).ready(function () {
                 }
             });
             
-        });
+        });    
+    }
 
+    // Function input: city to be changed to uppercase
+    // Function output: camel case city name
+    function cityToUpperCase(cityToChange) {
+        var cityUC = "";
+
+        cityToChange = cityToChange.split(" ");
+        // console.log(cityToChange);
+        cityToChange.forEach(function (cityPart) {
+            // console.log("before: ", cityPart);
+            cityPart = cityPart.charAt(0).toUpperCase() + cityPart.slice(1);
+            // console.log("after: ", cityPart);
+            cityUC = cityUC + " " + cityPart;
+            // console.log(cityUC);
+        });
+        return cityUC.trim();
+    }
+
+    $("#submit-btn").on("click", function (event) {
+        event.preventDefault();
+        // Assign variables to input values
+        var cityName = ($("#city-search").val()).toLowerCase().trim();
+        var stateName = ($("#state-search").val()).toUpperCase();
+        var fullStateName = getFullState(stateName);
+        // console.log(cityName, stateName, fullStateName)
+        $("input").val("");
+
+        if (cityName.length > 0 && stateName.length === 2) {
+            $(".modal-trigger").show();
+            ajaxCalls(cityName, stateName, fullStateName);
+        }
     });
+
+    
 });
 
 var statesArr = { "AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming" }
 
-
-var getFullState = function (stateAbbr) {
+function getFullState(stateAbbr) {
     return statesArr[stateAbbr]
-
 }
+// var getFullState = function (stateAbbr) {
+//     return statesArr[stateAbbr]
 
-// Graph Modal Controls
+//  Graph Modal Controls
 $(".modal-trigger").on("click", function(event) {
     event.preventDefault();
     $(".graph-modal").show();
 });
+
+
 $(".modal-close").click(function() {
     $(".modal").hide();
 });
